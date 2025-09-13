97c48634e020ed35c1d53ad8bb6596430a7aa3cf
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-09-13 13:28:24 +0100

    Exercise unsupported image types.

--- src/test/scala/ImageLoaderTests.scala
+++ src/test/scala/ImageLoaderTests.scala
@@ -4,7 +4,7 @@ import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
 
 class ImageLoaderTests extends AnyFlatSpec with GridUnderTest {
 
-  "Image loader sync end point" should "accept uploaded images and return upload status" in {
+  "Image loader sync end point" should "accept uploaded jpeg images and return upload status" in {
     val image = getClass.getResourceAsStream("IMG_4525.JPG").readAllBytes()
 
     val imageUploadResponse = gridApi.loadImage(image)
@@ -12,6 +12,25 @@ class ImageLoaderTests extends AnyFlatSpec with GridUnderTest {
 
     val uploadStatus = gridApi.getUploadStatus(imageUploadResponse.right.get.uri)
     uploadStatus.status mustBe "COMPLETED"
+  }
+
+  "Image loader sync end point" should "accept uploaded TIFF images and return upload status" in {
+    val image = getClass.getResourceAsStream("poppies.tif").readAllBytes()
+
+    val imageUploadResponse = gridApi.loadImage(image)
+    imageUploadResponse.isRight mustBe true
+
+    // TODO image/tif is not included in supported mime-types list. Need to understand why this is been accepted and gif is not
+    val uploadStatus = gridApi.getUploadStatus(imageUploadResponse.right.get.uri)
+    uploadStatus.status mustBe "COMPLETED"
+  }
+
+  "Image loader sync end point" should "reject unsupported image types" in {
+    val image = getClass.getResourceAsStream("Sunflower_as_gif_websafe.gif").readAllBytes()
+
+    val imageUploadResponse = gridApi.loadImage(image)
+    imageUploadResponse.isLeft mustBe true
+    imageUploadResponse.left.get mustBe "{\"errorKey\":\"unsupported-type\",\"errorMessage\":\"Unsupported mime-type: image/gif. Supported: image/jpeg, image/png\"}"
   }
 
   "Image loader sync end point" should "reject non image files" in {
