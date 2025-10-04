a0bf8faeffa40b60c6547e691398ca3acea111aa
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-10-04 20:47:27 +0100

    Highlight that non owned images with a syndicaiton lease can end up in the queued list.

--- src/test/scala/Fixtures.scala
+++ src/test/scala/Fixtures.scala
@@ -6,7 +6,6 @@ trait Fixtures {
   def gridApi: GridApi
 
   def uploadImage(filename: String): String = {
-    println(filename)
     val image = getClass.getResourceAsStream(filename).readAllBytes()
     val imageUploadResponse = gridApi.loadImage(image)
     val uploadStatusResponse = gridApi.getUploadStatusByURI(imageUploadResponse.right.get.uri)
--- src/test/scala/SyndicationTests.scala
+++ src/test/scala/SyndicationTests.scala
@@ -8,10 +8,13 @@ import java.util.UUID
 
 class SyndicationTests extends AnyFlatSpec with GridUnderTest with Fixtures {
 
-  val testImagesSet = getFilesInFolder("syndication")
+  private val testImagesSet = getFilesInFolder("syndication")
 
-  val forReview = testImagesSet.take(2)
-  val forQueued = testImagesSet.takeRight(4)
+  private val grouped = testImagesSet.grouped(2).toSeq
+  private val forReview = grouped.head
+  private val forQueued = grouped(1)
+  private val forNotOwned = grouped(2)
+  private val forNotOwnedButLeased = grouped(3)
 
   "Syndication filter" should "include owned images in review view" in {
     val imageIds = forReview.map { image =>
@@ -64,4 +67,58 @@ class SyndicationTests extends AnyFlatSpec with GridUnderTest with Fixtures {
     }
   }
 
+  it should "show non owned images as unsuitable" in {
+    val imageIds = forNotOwned.map { image =>
+      val imageUri = uploadImage(image)
+      val imageId = imageUri.split("/").last
+      imageId
+    }
+
+    imageIds.foreach { imageId =>
+      val newUsagesRights = Map(
+        "publication" -> "Test", // TODO how important is in that this matches config?
+        "category" -> "handout", // TODO source from API
+      )
+      gridApi.setUsageRights(imageId, newUsagesRights).isRight mustBe true
+    }
+
+    eventually(timeout(Span(5, Seconds)), interval(Span(100, Millis))) {
+      val images = gridApi.getImages(q = Some("+syndicationStatus:unsuitable"))
+      val imageIdsInSearchResponse = images.map(_.id)
+      imageIds.forall(imageIdsInSearchResponse.contains) mustBe true
+    }
+  }
+
+  it should "show non owned images with a syndication lease as unsuitable and not queued" in {
+    val imageIds = forNotOwnedButLeased.map { image =>
+      val imageUri = uploadImage(image)
+      val imageId = imageUri.split("/").last
+      imageId
+    }
+
+    imageIds.foreach { imageId =>
+      val newPhotographer = UUID.randomUUID().toString
+      val newUsagesRights = Map(
+        "publication" -> "Test", // TODO how important is in that this matches config?
+        "category" -> "handout", // TODO source from API
+        "photographer" -> newPhotographer
+      )
+      gridApi.setUsageRights(imageId, newUsagesRights).isRight mustBe true
+    }
+    imageIds.foreach { imageId =>
+      gridApi.addSyndicationLease(imageId)
+    }
+
+    eventually(timeout(Span(5, Seconds)), interval(Span(100, Millis))) {
+      val images = gridApi.getImages(q = Some("+syndicationStatus:unsuitable"))
+      val imageIdsInSearchResponse = images.map(_.id)
+      imageIds.forall(imageIdsInSearchResponse.contains) mustBe true
+    }
+
+    eventually(timeout(Span(5, Seconds)), interval(Span(100, Millis))) {
+      val images = gridApi.getImages(q = Some("+syndicationStatus:queued"))
+      val imageIdsInSearchResponse = images.map(_.id)
+      imageIds.forall(imageIdsInSearchResponse.contains) mustBe false
+    }
+  }
 }
