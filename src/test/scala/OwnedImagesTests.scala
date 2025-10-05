97fd66d8adeb60be7815940ed3d990a05a2639b5
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-10-05 10:33:06 +0100

    Exercise other owned images use cases.

--- src/test/scala/OwnedImagesTests.scala
+++ src/test/scala/OwnedImagesTests.scala
@@ -8,10 +8,15 @@ import java.util.UUID
 
 class OwnedImagesTests extends AnyFlatSpec with GridUnderTest with Fixtures {
 
-  val testImagesSet = Set("IMG_3938.JPG", "IMG_3938.JPG", "IMG_4470.JPG")
+  val testImagesSet = getFilesInFolder("owned")
+  private val grouped = testImagesSet.grouped(1).toSeq
+  private val forStaffPhotographer = grouped.head
+  private val forContractPhotographer = grouped(1)
+  private val forCommissionedPhotographer = grouped(2)
+  private val forNonOwned = grouped(3)
 
   "Owned images" should "include images with staff photographer rights" in {
-    val images = testImagesSet.map { filename =>
+    val images = forStaffPhotographer.map { filename =>
       uploadImage(filename)
     }
 
@@ -26,9 +31,77 @@ class OwnedImagesTests extends AnyFlatSpec with GridUnderTest with Fixtures {
     }
 
     eventually(timeout(Span(5, Seconds)), interval(Span(100, Millis))) {
-      val images = gridApi.getImages(q = Some("is:owned"))
-      val imageIdsInSearchResponse = images.map(_.id)
+      val imageIdsInSearchResponse = gridApi.getImages(q = Some("is:owned")).map(_.id)
       images.map(_.id).forall(imageIdsInSearchResponse.contains) mustBe true
+    }
+  }
+
+  it should "include images with commissioned photographer rights" in {
+    val images = forCommissionedPhotographer.map { filename =>
+      uploadImage(filename)
+    }
+
+    images.foreach { image =>
+      val newPhotographer = UUID.randomUUID().toString
+      val newUsagesRights = Map(
+        "publication" -> "Test", // TODO how important is in that this matches config?
+        "category" -> "commissioned-photographer", // TODO source from API
+        "photographer" -> newPhotographer
+      )
+      gridApi.setUsageRights(image.id, newUsagesRights).isRight mustBe true
+    }
+
+    eventually(timeout(Span(5, Seconds)), interval(Span(100, Millis))) {
+      val imageIdsInSearchResponse = gridApi.getImages(q = Some("is:owned")).map(_.id)
+      images.map(_.id).forall(imageIdsInSearchResponse.contains) mustBe true
+    }
+
+  }
+
+  it should "include images with contract photographer rights" in {
+    val images = forContractPhotographer.map { filename =>
+      uploadImage(filename)
+    }
+
+    images.foreach { image =>
+      val newPhotographer = UUID.randomUUID().toString
+      val newUsagesRights = Map(
+        "publication" -> "Test", // TODO how important is in that this matches config?
+        "category" -> "contract-photographer", // TODO source from API
+        "photographer" -> newPhotographer
+      )
+      gridApi.setUsageRights(image.id, newUsagesRights).isRight mustBe true
+    }
+
+    eventually(timeout(Span(5, Seconds)), interval(Span(100, Millis))) {
+      val imageIdsInSearchResponse = gridApi.getImages(q = Some("is:owned")).map(_.id)
+      images.map(_.id).forall(imageIdsInSearchResponse.contains) mustBe true
+    }
+  }
+
+  it should "not include images non owned usages rights" in {
+    val images = forNonOwned.map { filename =>
+      uploadImage(filename)
+    }
+
+    images.foreach { image =>
+      val newPhotographer = UUID.randomUUID().toString
+      val newUsagesRights = Map(
+        "publication" -> "Test", // TODO how important is in that this matches config?
+        "category" -> "handout", // TODO source from API
+        "photographer" -> newPhotographer
+      )
+      gridApi.setUsageRights(image.id, newUsagesRights).isRight mustBe true
+    }
+
+    eventually(timeout(Span(5, Seconds)), interval(Span(100, Millis))) {
+      val imageIdsInSearchResponse = gridApi.getImages(q = Some("-is:owned")).map(_.id)
+      images.map(_.id).forall(imageIdsInSearchResponse.contains) mustBe true
+    }
+
+    eventually(timeout(Span(5, Seconds)), interval(Span(100, Millis))) {
+      val imageIdsInSearchResponse = gridApi.getImages(q = Some("is:owned")).map(_.id)
+      images.map(_.id).forall(imageIdsInSearchResponse.contains) mustBe false
     }
   }
 
